81fd52426b1ecad2b21783a0e4712a80734ea5f3
Danilo Humberto <danilooliveira@softbox.com.br>
2017-08-14 18:45:00 -0300

    Commit inicial

---
+++ src/main/java/br/com/controller/ApiDinamicaRestController.java
@@ -0,0 +1,69 @@
+package br.com.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.service.RegistroModeloService;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class ApiDinamicaRestController {
+
+	@Autowired
+	private RegistroModeloService registroModeloService;
+
+	@RequestMapping(value = "/{modelo}", method = RequestMethod.GET)
+	public ResponseEntity getAll(@PathVariable String modelo) {
+		try {
+			return ResponseEntity.ok(registroModeloService.buscarTodos(modelo));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/{modelo}/{id}", method = RequestMethod.GET)
+	public ResponseEntity getById(@PathVariable String modelo, @PathVariable String id) {
+		try {
+			return ResponseEntity.ok(registroModeloService.buscarPorId(modelo, id));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/{modelo}", method = RequestMethod.POST)
+	public ResponseEntity create(@PathVariable String modelo, @RequestBody Map<String, Object> atributos) {
+		try {
+			return ResponseEntity.ok(registroModeloService.criar(modelo, atributos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/{modelo}/{id}", method = RequestMethod.PUT)
+	public ResponseEntity updateById(@PathVariable String modelo, @PathVariable String id,
+			@RequestBody Map<String, Object> atributos) {
+		try {
+			return ResponseEntity.ok(registroModeloService.atualizar(modelo, id, atributos));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+	@RequestMapping(value = "/{modelo}/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity deleteById(@PathVariable String modelo, @PathVariable String id) {
+		try {
+			return ResponseEntity.ok(registroModeloService.removerPorId(modelo, id));
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		}
+	}
+
+}
