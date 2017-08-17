ec91364e96c32bdb5b6e3c20c831179e3c5cc738
Danilo Humberto <danilooliveira@softbox.com.br>
2017-08-17 13:57:33 -0300

    Commit inicial

--- src/main/java/br/com/xyinc/persistence/ModeloRepositorio.java
+++ src/main/java/br/com/xyinc/persistence/ModeloRepositorio.java
@@ -1,11 +1,13 @@
 package br.com.xyinc.persistence;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import br.com.xyinc.domain.Modelo;
 
 public interface ModeloRepositorio extends MongoRepository<Modelo, String> {
 
+	@Query("{nome: ?0}")
 	Modelo buscarPorNome(String nome);
 }
 
